3d098dcd63828deb8aa39eabff126f55ee630193
Armand Adroher <aadroher@gmail.com>
2020-11-19 22:22:23 +0100

    First steps.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game of Rife
+
+First attempts at writing Conway's Game of Life in Rust.
--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,5 @@
 use im::HashSet;
 
-#[derive(Debug)]
 type Cell = (i64, i64);
 
 #[derive(Debug)]
@@ -8,14 +7,76 @@ pub struct World {
   cells: HashSet<Cell>,
 }
 
-pub fn new_world(cells: Cell[]) -> World {
+pub fn new_world(cells: Vec<Cell>) -> World {
   World {
-    cells
+    cells: HashSet::from(cells),
   }
 }
 
+fn with_neighbours(cell: &Cell) -> HashSet<Cell> {
+  let (x, y) = cell;
+  HashSet::from(vec![
+    (x - 1, y + 1),
+    (x + 0, y + 1),
+    (x + 1, y + 1),
+    (x - 1, y + 0),
+    (x + 0, y + 0),
+    (x + 1, y + 0),
+    (x - 1, y - 1),
+    (x + 0, y - 1),
+    (x + 1, y - 1),
+  ])
+}
+
+fn get_candidates(cells: &HashSet<Cell>) -> HashSet<Cell> {
+  let with_repetition: Vec<Cell> = cells
+    .into_iter()
+    .flat_map(|cell| with_neighbours(cell))
+    .collect();
+  HashSet::from(with_repetition)
+}
+
+fn get_num_neighbours(world_cells: &HashSet<Cell>, cell: &Cell) -> usize {
+  let (x, y) = cell;
+  let neighbour_positions = vec![
+    (x - 1, y + 1),
+    (x + 0, y + 1),
+    (x + 1, y + 1),
+    (x - 1, y + 0),
+    (x + 1, y + 0),
+    (x - 1, y - 1),
+    (x + 0, y - 1),
+    (x + 1, y - 1),
+  ];
+  neighbour_positions
+    .into_iter()
+    .filter(|cell| world_cells.contains(cell))
+    .count()
+}
+
+pub fn next_tick(world: World) -> World {
+  let World { cells } = world;
+  let candidates = get_candidates(&cells);
+  let survivors: HashSet<Cell> = candidates
+    .into_iter()
+    .filter(|cell| get_num_neighbours(&cells, &cell) == 3)
+    .collect();
+  World { cells: survivors }
+}
 
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn it_works() {
+    let initial_cells = vec![(-1, 0), (0, 0), (0, 1)];
+    let world = new_world(initial_cells);
+    let World { cells: next_cells } = next_tick(world);
+    let expected_next_cells: HashSet<Cell> = HashSet::from(vec![(1, 0), (0, 0), (0, 1)]);
+
+    println!("{:?}", next_cells);
+
+    assert_eq!(next_cells, expected_next_cells);
+  }
 }
