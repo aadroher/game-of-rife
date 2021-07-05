0166722d4de1d8e63b90bc95eeb6dea020e72975
Armand Adroher <aadroher@gmail.com>
2021-07-05 22:57:26 +0200

    Adds some mock bin body.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-mod world;
+use game_of_rife::world::World;
+use im::hashset;
 
 fn main() {
   println!("Hello, world!");
+
+  let initial_cells = hashset![(0, 0), (1, 0), (2, 0), (2, 1), (1, 2)];
+  let world0 = World {
+    cells: initial_cells,
+  };
+  println!("{:?}", world0);
+
+  let world1 = world0.forward(100000);
+  println!("{:?}", world1);
 }
